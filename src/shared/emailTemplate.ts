300ffa47f89ab9a66f0736ad134df6275355aeb3
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-11-22 11:18:19 +0600

    fixing some issues

--- src/shared/emailTemplate.ts
+++ src/shared/emailTemplate.ts
@@ -8,8 +8,8 @@ const createAccount = (values: ICreateAccount) => {
     subject: 'Verify your account',
     html: `<body style="font-family: Arial, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
     <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-        <img src="${process.env.PRODUCTION_URL}/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
-          <h2 style="color: #64bae7; font-size: 24px; margin-bottom: 20px;">Hey! ${values.name}, Your Toothlens Account Credentials</h2>
+       
+          <h2 style="color: #64bae7; font-size: 24px; margin-bottom: 20px;">Hey! ${values.name}, Your Medspace Connect Account Credentials</h2>
         <div style="text-align: center;">
             <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
             <div style="background-color: #64bae7; width: 80px; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${values.otp}</div>
@@ -20,14 +20,14 @@ const createAccount = (values: ICreateAccount) => {
   };
   return data;
 };
-
+//  <img src="${process.env.PRODUCTION_URL}/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
 const resetPassword = (values: IResetPassword) => {
   const data = {
     to: values.email,
     subject: 'Reset your password',
     html: `<body style="font-family: Arial, sans-serif; background-color: #f9f9f9; margin: 50px; padding: 20px; color: #555;">
     <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-        <img src="${process.env.PRODUCTION_URL}/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
+       
         <div style="text-align: center;">
             <p style="color: #555; font-size: 16px; line-height: 1.5; margin-bottom: 20px;">Your single use code is:</p>
             <div style="background-color: #64bae7; width: 80px; padding: 10px; text-align: center; border-radius: 8px; color: #fff; font-size: 25px; letter-spacing: 2px; margin: 20px auto;">${values.otp}</div>
@@ -38,6 +38,7 @@ const resetPassword = (values: IResetPassword) => {
   };
   return data;
 };
+// / <img src="${process.env.PRODUCTION_URL}/logo.png" alt="Logo" style="display: block; margin: 0 auto 20px; width:150px" />
 const aUserInterested = (
   username: string,
   providername: string,
