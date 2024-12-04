65600b14c2297e6db671986d7ab79bd51fe62370
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-12-04 17:41:18 +0600

    adding website ip in cors

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,6 @@ app.use(
   cors({
     origin: 'https://medspaceconnect.com',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 //morgan
